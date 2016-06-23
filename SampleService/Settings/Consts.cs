3a647ed99702fcfa33ea9e8a64ade7028184c842
Andrey-Anatolyevich <cccpmade@gmail.com>
2016-06-23 14:19:49 +0300

    Update Consts.cs

--- SampleService/Settings/Consts.cs
+++ SampleService/Settings/Consts.cs
@@ -12,12 +12,6 @@
 		///<summary> Config keys </summary>
 		public static class Config
 		{
-			/// <summary> [TimerDelay] Timer delay </summary>
-			public const string TimerDelay = "TimerDelay";
-
-			/// <summary> [OnExceptionDelay] Exception delay </summary>
-			public const string OnExceptionDelay = "OnExceptionDelay";
-
 			///<summary> [:] Separator for delay strings </summary>
 			public const char DelayStringSeparator = ':';
 		}
